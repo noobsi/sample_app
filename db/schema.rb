098470913dc3d0930430d6c89aaf907de5b13511
noobsi <quocyoghurt@gmail.com>
2018-07-19 15:30:03 +0700

    Chapter7

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2018_07_23_080052) do
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,6 +17,6 @@ ActiveRecord::Schema.define(version: 2018_07_23_080052) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
-
 end
